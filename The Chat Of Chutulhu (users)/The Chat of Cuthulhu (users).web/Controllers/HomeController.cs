0ef13a1d79a56815703ea4402cfc4c8d13aeae22
Montoya45 <Montoya45@DESKTOP-SUON9BA>
2019-08-27 12:21:13 -0600

    resubiendo todo

---
+++ The Chat Of Chutulhu (users)/The Chat of Cuthulhu (users).web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace The_Chat_of_Cuthulhu__users_.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ The Chat Of Chutulhu (users)/The Chat of Cuthulhu (users).web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="The_Chat_of_Cuthulhu__users_.web.MvcApplication" Language="C#" %>
