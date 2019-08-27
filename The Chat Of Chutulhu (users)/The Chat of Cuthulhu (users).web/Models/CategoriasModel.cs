decef22a232cf5004d643d49b325e4ef5fb1a12b
Montoya45 <Montoya45@DESKTOP-SUON9BA>
2019-08-27 13:07:13 -0600

    controladores y categorias en proceso

---
+++ The Chat Of Chutulhu (users)/The Chat of Cuthulhu (users).web/Models/CategoriasModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_Chat_of_Cuthulhu__users_.web.Models
+{
+    public class CategoriasModel
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
